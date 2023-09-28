8eadf9fa427ce8b349cc27ef2ef5a673b1d8819a
Bui Van Anh <buivananh01072003@gmail.com>
2023-09-28 16:25:04 +0700

    fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import router from './routes/index.js'
 import 'dotenv/config'
 import cors from 'cors';
 const corsOptions ={
-  origin:'http://127.0.0.1:3000', 
+  origin:'https://tiktok-clone-rouge.vercel.app', 
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200,
 }
@@ -18,7 +18,7 @@ app.use(cookieParser())
 app.use(cors(corsOptions));
 app.use(morgan('dev'));
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
+  res.header('Access-Control-Allow-Origin', 'https://tiktok-clone-rouge.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
